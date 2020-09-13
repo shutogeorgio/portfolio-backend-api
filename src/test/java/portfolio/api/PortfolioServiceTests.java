c379690a52c761e9dd8f8c36fb2fe2c20ff896e3
shuGeorgio <dev.shudiscrete@gmail.com>
2020-09-13 23:48:27 +0900

    Add: test init

--- src/test/java/portfolio/api/PortfolioServiceTests.java
+++ src/test/java/portfolio/api/PortfolioServiceTests.java
@@ -1,32 +1,33 @@
-//package portfolio.api;
+package portfolio.api;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+
+@DataJpaTest
+@ActiveProfiles("ut")
+public class PortfolioServiceTests {
+
+    @MockBean
+    private PortfolioService portfolioService;
+
+    @MockBean
+    private PortfolioRepository portfolioRepository;
+
+    @BeforeEach
+    public void setup() {
+    }
+
+//    @Test
+//    public void getAllShouldReturnOk() throws Exception {
+//        Portfolio portfolio = new Portfolio();
+//        BDDMockito.given(portfolioRepository.findAll()).willReturn(List.of(portfolio));
 //
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.mockito.BDDMockito;
-//import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.boot.test.mock.mockito.MockBean;
-//
-//import java.util.List;
-//
-//@SpringBootTest
-//@DataJpaTest
-//public class PortfolioServiceTests {
-//
-//    @MockBean
-//    private PortfolioService portfolioService;
-//
-//    @MockBean
-//    private PortfolioRepository portfolioRepository;
-//
-//    @BeforeEach
-//    public void setup() {
 //    }
-//
-////    @Test
-////    public void getAllShouldReturnOk() throws Exception {
-////        Portfolio portfolio = new Portfolio();
-////        BDDMockito.given(portfolioRepository.findAll()).willReturn(List.of(portfolio));
-////
-////    }
-//}
+}
