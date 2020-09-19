d18a9f3f16eb23c6e399a2428686b3e68995b79a
shuGeorgio <dev.shudiscrete@gmail.com>
2020-09-20 01:36:07 +0900

    Add: testing

--- src/main/java/portfolio/api/config/WebConfig.java
+++ src/main/java/portfolio/api/config/WebConfig.java
@@ -1,9 +1,17 @@
 package portfolio.api.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -11,8 +19,16 @@ public class WebConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().anyRequest().authenticated().and().formLogin().and()
-				.httpBasic().and().csrf().disable();
+		http.cors().and().authorizeRequests().anyRequest().authenticated().and()
+				.formLogin().and().httpBasic().and().csrf().disable();
+	}
+
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**",
+				new CorsConfiguration().applyPermitDefaultValues());
+		return source;
 	}
 
 }
