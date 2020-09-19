72d802a89c3edb5790b6cdf86b52d064031683b1
shuGeorgio <dev.shudiscrete@gmail.com>
2020-09-20 00:35:03 +0900

    fix: cors setting

--- src/main/java/portfolio/api/controller/PortfolioController.java
+++ src/main/java/portfolio/api/controller/PortfolioController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 import static org.springframework.http.ResponseEntity.ok;
 
-@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/api/v1/")
 public class PortfolioController {
