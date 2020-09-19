7de574d1876234b0caea6e8916cc3959b8c8dc54
shuGeorgio <dev.shudiscrete@gmail.com>
2020-09-19 22:39:39 +0900

    Add: cors setting

--- src/main/java/portfolio/api/config/WebMvcConfig.java
+++ src/main/java/portfolio/api/config/WebMvcConfig.java
@@ -6,9 +6,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000");
-    }
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/api/**").allowedOrigins("http://localhost:3000");
+	}
+
 }
